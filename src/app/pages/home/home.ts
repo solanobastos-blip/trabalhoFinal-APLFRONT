23a6d167b9437ca3f4b23de4f6253d209558730b
Aluno Canoas <aluno.canoas@ensino.ulbra.net>
2026-06-16 21:46:58 -0300

    segundocommit

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,43 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { JsonPipe } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule],
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
 })
 export class Home {
-  formTeste!: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.formTeste = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]]
-    });
-
-    const nameTest = this.formTeste.get('name');
-
-    if (nameTest?.hasError('required')) {
-      console.log('Faltou preencher o nome!');
-    }
-
-    if (nameTest?.hasError('minlength')) {
-      console.log('Nome muito curto!');
-    }
-  }
-
-  get name() {
-    return this.formTeste.get('name') as FormControl;
-  }
-
-  get email() {
-    return this.formTeste.get('email') as FormControl;
-  }
-
-  submitForm() {
-    console.log(this.formTeste.value);
-  }
-
+  
 }
